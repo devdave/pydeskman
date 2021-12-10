f4da5134e5a79c4fc80404e9e99e759c23254b98
devdave <devdave@ominian.net>
2021-12-09 18:35:55 -0700

    initial block and tackle/outlining on T3 interface

--- protos/t3/t3.py
+++ protos/t3/t3.py
@@ -1,6 +1,25 @@
-
+import sys
+from pathlib import Path
 
 from pydeskman import GenerateApp
+from pydeskman import QObject
+
+class GameConnection(QObject):
+
+    def wantsApplication(self, app):
+        self.app = app
 
 
 
+
+
+def main(argv):
+
+    main_view = (Path(__file__).parent / 'view' / 'main.html')
+    assert main_view.exists()
+
+    GenerateApp("Tic-tac-toe", dict(height=400, width=350), main_view, GameConnection(), main_view.parent, enable_debug=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
