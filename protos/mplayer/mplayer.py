2805e2f3d4767b7ee9039a827534a3c0f0415951
devdave <devdave@ominian.net>
2021-12-23 11:13:03 -0700

    Stub/skeleton work

--- protos/mplayer/mplayer.py
+++ protos/mplayer/mplayer.py
@@ -3,17 +3,69 @@ from pathlib import Path
 from pydeskman import GenerateApp
 from pydeskman import QObject, Slot, Signal, Property
 
+from pybass3 import Song
+
+
+
+
+
+
+class MusicManager:
+
+    _current_song: Song
+
+    def __init__(self):
+        self._current_song = None
+
+    def load_song(self, song_path):
+        self._current_song = Song(song_path)
+
+    def play(self):
+        assert self._current_song is not None
+        self._current_song.play()
+
+    def stop(self):
+        assert self._current_song is not None
+        self._current_song.stop()
+
+    def pause(self):
+        assert self._current_song is not None
+        self._current_song.pause()
+
+
 class Switchboard(QObject):
+
+    manager: MusicManager
 
     def __init__(self, music_manager):
         self.manager = music_manager
 
         QObject.__init__(self)
 
+    @Slot(result=bool)
+    def request_load_dlg(self):
+        pass
 
+    @Slot(result=bool)
+    def play(self):
+        self.manager.play()
 
-class MusicManager:
-    pass
+    @Slot(result=bool)
+    def stop(self):
+        self.manager.stop()
+
+    @Slot(result=bool)
+    def pause(self):
+        self.manager.pause()
+
+    @Slot(int, result=bool)
+    def seek(self, position):
+        """
+
+        :param position: 0 to 100
+        :return:
+        """
+        pass
 
 
 
