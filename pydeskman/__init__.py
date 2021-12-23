4b5600d39bb6726fd2459f79dbf33ed3db772b88
devdave <devdave@ominian.net>
2021-12-22 22:44:12 -0700

    expose a couple more QT5 components/classes

--- pydeskman/__init__.py
+++ pydeskman/__init__.py
@@ -1,4 +1,7 @@
 from .generator import GenerateApp
 
 from PySide2.QtCore import QObject
+from PySide2.QtWidgets import QWidget
+from PySide2.QtGui import QWindow
+from PySide2.QtWebEngineWidgets import QWebEnginePage
 from PySide2.QtCore import Property, Slot, Signal
