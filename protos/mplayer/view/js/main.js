6572e9d560dd2134f5be3a301635d51b60d9deff
devdave <devdave@ominian.net>
2021-12-29 09:41:58 -0700

    Manage the app through a singleton class

---
+++ protos/mplayer/view/js/main.js
@@ -0,0 +1,89 @@
+
+class MPlayerLogic {
+    constructor(switchboard, channel) {
+        this.switchboard = switchboard;
+        this.channel = channel;
+        console.log("MPlayer constructed");
+
+        this.song_length = null;
+
+        // player elements
+        this.position_elm = null;
+
+    }
+
+
+
+    setup(document, window) {
+        document.getElementById("open_song").addEventListener("click", evt => this.switchboard.request_load_dlg().then(this.on_file_load.bind(this)));
+
+        this.switchboard.newSong.connect(this.on_new_song.bind(this));
+        this.switchboard.songUpdate.connect(this.on_song_update.bind(this));
+
+        console.log("MPlayer setup");
+
+        this.switchboard.currentSong().then(this.on_check_current_song.bind(this));
+
+        document.getElementById("play").addEventListener("click", evt => this.switchboard.play());
+        document.getElementById("pause").addEventListener("click", evt => this.switchboard.pause());
+        document.getElementById("stop").addEventListener("click", evt => this.switchboard.stop());
+
+        this.position_elm = document.getElementById("position");
+        this.title_elm = document.querySelector("#player_body .title");
+        this.artist_elm = document.querySelector("#player_body .artist");
+        this.length_elm = document.querySelector("#player_body .length");
+
+    }
+
+    on_file_load(meta) {
+        console.log("Song loaded: ", meta);
+    }
+
+    on_check_current_song(raw) {
+        let response = JSON.parse(raw);
+
+        if (response.status === true) {
+            this.set_title(response.meta.title);
+            this.set_artist(response.meta.artist);
+            this.set_length(response.length.string);
+        }
+
+    }
+
+    on_new_song(raw) {
+        let data = JSON.parse(raw)
+        console.log("There is a new song loaded", data.meta);
+        this.set_title(data.meta.title);
+        this.set_artist(data.meta.artist);
+        this.set_length(data.length.string);
+
+        this.song_length = data.length.seconds;
+    }
+
+    on_song_update(position, is_playing) {
+        // TODO - this math is a tad hairy and I know there is a simpler equation
+        let perc = 100 - (((this.song_length - position)/this.song_length) * 100)
+
+
+        console.log("Song is @ ", perc, "and is playing:", is_playing);
+        this.set_song_position(perc);
+
+    }
+
+    set_song_position(position){
+        this.position_elm.value = position
+    }
+
+    set_title(title) {
+        this.title_elm.innerHTML = title;
+    }
+
+    set_artist(artist) {
+        this.artist_elm.innerHTML = artist;
+    }
+
+    set_length(length) {
+        this.length_elm.innerHTML = length;
+    }
+
+}
