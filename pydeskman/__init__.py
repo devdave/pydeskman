911edf937b8e3a6a4d90b8dfb8400219727702d5
devdave <devdave@ominian.net>
2021-12-29 09:35:56 -0700

    Expose QTimer

--- pydeskman/__init__.py
+++ pydeskman/__init__.py
@@ -4,4 +4,4 @@ from PySide2.QtCore import QObject
 from PySide2.QtWidgets import QWidget
 from PySide2.QtGui import QWindow
 from PySide2.QtWebEngineWidgets import QWebEnginePage
-from PySide2.QtCore import Property, Slot, Signal
+from PySide2.QtCore import Property, Slot, Signal, QTimer
